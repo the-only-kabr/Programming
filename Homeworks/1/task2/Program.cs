c7c5c81645c96a543b4d084e6ff0faffc565afad
zlokabr <zlokabr@gmail.com>
2022-10-19 22:24:04 +0300

    2nd task done

---
+++ Homeworks/1/task2/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите первое число");
+int i = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int j = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int k = int.Parse(Console.ReadLine());
+
+int max = i;
+
+if (j > max) max = j;
+if (k > max) max = k;
+
+Console.WriteLine($"Максимальное число = {max}");
