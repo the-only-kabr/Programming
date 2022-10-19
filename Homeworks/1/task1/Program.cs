b37b05f1fab7faae5a59bc0ecd52d1c0feb79812
zlokabr <115564552+zlokabr@users.noreply.github.com>
2022-10-19 21:23:49 +0300

    1st task done

--- Homeworks/1/task1/Program.cs
+++ Homeworks/1/task1/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите первое число");
+int i = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int j = int.Parse(Console.ReadLine());
+
+    if (i < j)
+{
+    Console.WriteLine($"Наименьшее число {i} , а наибольшее = {j}"); 
+    
+}
+    else
+{
+    Console.WriteLine($"Наименьшее число {j} , а наибольшее = {i}");
+
+}
+    
