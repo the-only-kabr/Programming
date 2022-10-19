734f853bfba0e06559c773b7ae5b9c1d6a3ecfcd
zlokabr <zlokabr@gmail.com>
2022-10-19 22:33:44 +0300

    3rd task done

---
+++ Homeworks/1/task3/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число");
+int i = int.Parse(Console.ReadLine());
+
+ if (i % 2 == 0)
+{
+    Console.WriteLine("Это четное число"); 
+    
+}
+    else
+{
+    Console.WriteLine("Это нечетное число");
+
+}
