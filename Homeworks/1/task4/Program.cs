b2501e5f7ae08d6131da4cec9200ac6f88b2d58b
kabr <zlokabr@gmail.com>
2022-10-20 08:27:59 +0300

    4th task done

---
+++ Homeworks/1/task4/Program.cs
@@ -0,0 +1,14 @@
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Insert number");
+int n = int.Parse(Console.ReadLine());
+
+// int i = 0;
+
+//	if (i<=n)
+//Console.WriteLine(i);
+//i+=2;
+
+	for (int i = 1; i <= n; i++)
+	if (i % 2 == 0)
+		Console.WriteLine(i);
+//	i += 2;
