b647594898246d50679d07ed6359dae059c62279
zlokabr <zlokabr@gmail.com>
2022-10-25 06:58:23 +0300

    4th seminar 2nd task done

--- Homeworks/4/task2/Program.cs
+++ Homeworks/4/task2/Program.cs
@@ -5,10 +5,22 @@
  82 -> 10
 
  9012 -> 12*/
-Console.WriteLine("Insert number");
-string x = Console.ReadLine();
-int sum = 0;
+/*Console.WriteLine("Insert number");
+String x = (Console.ReadLine());
+//Char[]Numbers = x.ToCharArray();
+int[] Digits = new int[x.Length]
+//int sum = 0;
     for (int i = 0; i < x.Length; i++)
-        sum = sum + x[i];
+        Digits[I] = Convert.ToInt32(Numbers[I]);
 
-Console.WriteLine($"Inner numbers sum is = {sum}");
+//Console.WriteLine($"Inner numbers sum is = {sum}");
+*/
+Console.WriteLine("Insert number");
+int x = int.Parse(Console.ReadLine());
+int sum=0;
+for (int i=3; i>=0; i--)
+{
+    sum+=x/(int)Math.Pow(10.0,i);
+    x=x%(int)Math.Pow(10.0,i);
+}
+Console.WriteLine($"Number's digits sum = {sum}");
