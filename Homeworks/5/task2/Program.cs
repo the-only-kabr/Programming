682cb5b15e661928e83d66179e04ed9913ee7ef3
zlokabr <zlokabr@gmail.com>
2022-10-26 21:28:19 +0300

    5th seminar 2nd task done

---
+++ Homeworks/5/task2/Program.cs
@@ -0,0 +1,42 @@
+﻿/*Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях. 
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+int[] array = new int[10];
+
+int[] CreateArray(int[] array)
+{
+	
+
+	Random random = new Random();
+
+	for (int i = 0; i < array.Length; i++)
+	{
+		array[i] = random.Next(1, 20);
+	}
+	return array;
+}
+
+void PrintArray(int[] array)
+{
+	for (int i = 0; i < array.Length; i++)
+			{
+			Console.Write(array[i] + " ");
+		}
+		Console.WriteLine();
+}
+CreateArray(array);
+PrintArray(array);
+Console.WriteLine($"Sum of odd elements = {GetSum(array)} ");
+int GetSum(int[] array)
+{
+	//int sum = array[0];
+	int sum = 0;
+	for (int i = 0; i < array.Length; i++)
+		if (i % 2 != 0)
+		{ 
+			sum = sum + array[i] ;
+			
+		}
+	return sum;
+	}
---
+++ Seminars/8/1/Program.cs
@@ -0,0 +1,52 @@
+﻿/*/ See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+*/
+
+Console.WriteLine("Введите количество значений: ");
+int m = int.Parse(Console.ReadLine());
+int[] array = GetArray(m);
+int[,] countarray = CountNumbers(array);
+PrintArray(countarray);
+int[] GetArray(int m)
+
+{
+    int[] array = new int[m];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+            array[i] = rnd.Next(1, 10);
+     
+    }
+    return array;
+}
+
+int[,] CountNumbers(int[] array)
+{
+int count = 0;
+int[,] countarray = new int[array.Length, 2];
+    for (int i = 0; i < array.Length; i++)
+    {
+        countarray[i, 0] = array[i];
+    }
+    for (int j = 0; j < array.Length; j++)
+    { 
+    if (countarray[j, 0] == array[j])
+        { count++;
+            countarray[j, 1] = count;
+        }
+    }
+    return countarray;
+}
+
+void PrintArray(int[,] array2)
+{
+    for (int i = 0; i < array2.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2.GetLength(1); j++)
+        {
+            Console.Write($"{array2[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+
+}
