91edb0647482d76774b2fdc036b86046e1f6b259
zlokabr <zlokabr@gmail.com>
2022-10-26 23:07:13 +0300

    5th seminar 1st task done

--- Homeworks/5/task1/Program.cs
+++ Homeworks/5/task1/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2*/
+
+int[] array = new int[10];
+
+int[] CreateArray(int[] array)
+{
+	Random random = new Random();
+
+	for (int i = 0; i < array.Length; i++)
+	{
+		array[i] = random.Next(100, 999);
+	}
+	return array;
+}
+
+void PrintArray(int[] array)
+{
+	for (int i = 0; i < array.Length; i++)
+			{
+			Console.Write(array[i] + " ");
+		}
+		Console.WriteLine();
+}
+CreateArray(array);
+PrintArray(array);
+GetEvenCount(array);
+
+Console.WriteLine($"Count of even numbers is = {GetEvenCount(array)} ");
+int GetEvenCount(int[] array)
+{
+	int count = 0;
+	for (int i = 0; i < array.Length; i++)
+		if ((array[i] % 2) == 0)
+		{ 
+			count++;
+		}
+		return count;
+}
