30f17c116664d3362daa1fc012b3c423b2a55f50
zlokabr <zlokabr@gmail.com>
2022-10-27 22:58:52 +0300

    1st task 6 seminar in the middle

--- Homeworks/6/task1/Program.cs
+++ Homeworks/6/task1/Program.cs
@@ -5,7 +5,7 @@
  1, -7, 567, 89, 223-> 3
 */
 
-Console.Write("Input numbers(w/ _ btw): ");
+/*Console.Write("Input numbers(w/ _ btw): ");
 int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 int count = 0;
  
@@ -19,3 +19,83 @@ for (int i = 0; i < array.Length; i++)
  
 Console.WriteLine($"Amount of numbers over null = {count}");
 
+*/
+/*/ See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+*/
+/*
+Console.Write("Введите элементы(через пробел): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+ Console.WriteLine($"Кол-во элементов > 0: {count}");
+*/
+Console.Write("Введите числа через запятую: ");
+int[] numbers = StringToNum(Console.ReadLine());
+PrintArray(numbers);
+int sum = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > 0)
+    {
+        sum++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"количество значений больше 0 = {sum}");
+
+
+int[] StringToNum(string input)
+{
+    int count = 1;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] == ',')
+        {
+            count++;
+        }
+    }
+
+    int[] numbers = new int [count];
+    int index = 0;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        string temp = "";
+
+        while (input [i] != ',')
+        {
+        if(i != input.Length - 1)
+        {
+            temp += input [i].ToString();
+            i++;
+        }
+        else
+        {
+            temp += input [i].ToString();
+            break;
+        }
+        }
+        numbers[index] = Convert.ToInt32(temp);
+        index++;
+    }
+    return numbers;
+}
+
+
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.Write("]");
+}
