b477bfb9c11693911dd5e08083780e9409d3675f
zlokabr <zlokabr@gmail.com>
2022-10-27 06:28:03 +0300

    bf merge

--- Homeworks/5/task1/Program.cs
+++ Homeworks/5/task1/Program.cs
@@ -2,37 +2,42 @@
 
 [345, 897, 568, 234] -> 2*/
 
-int[] array = new int[20];
-Random rnd = new Random();
-	/*for (int i = 0; i < array.Length; i++)
-		{
-		array[i] = rnd.Next(100, 999);
-		Console.WriteLine(array[i]);
-		}*/
-string ArrayToString(int[] array)
+Console.WriteLine("Insert number of array elements = ");
+Console.WriteLine();
+int n = int.Parse(Console.ReadLine());
+int[] CreateArray()
 {
-	string result = string.Empty;
-
-	for (int i = 0; i < array.Length; i++)
-	{
-		array[i] = rnd.Next(100, 999);
-		result += $"{array[i]}, ";
-		
-	}
-
-	return result;
-}
-Console.WriteLine(ArrayToString(array));
-
-int FindCount(int[] array)
-{
-    int count = 0;
-    for (int i = 0; i < array.Length; i++)
-        {
-        if (array[i] % 2 = 0);
-            count++;
+    int[] array = new int[n];
+    Random r = new Random();
+    for (int i = 0; i < n; i++)
+    {
+            array[i] = r.Next(100, 999);
         }
-    return count;
+    return array;
 }
 
-Console.WriteLine(FindCount(array));
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < n; i++)
+    {
+    Console.Write(array[i] + " ");
+        Console.WriteLine();
+    }
+}
+
+void FindEvens(int[], array)
+int result = 0;
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (array[i] % 2 = 0)
+        {
+            result +=        
+        }
+    }
+    return result;
+}
+
+int[] array = CreateArray();
+PrintArray(array);
+Console.WriteLine();
