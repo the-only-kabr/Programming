914679b7f05c2ffd6446eb76b58cb6e6625b1a6c
zlokabr <zlokabr@gmail.com>
2022-10-27 22:31:45 +0300

    seminar 9

---
+++ Seminars/9/3/Program.cs
@@ -0,0 +1,24 @@
+﻿/*/ рекурсия стрингом
+*/
+using System.Text;
+
+StringBuilder GetNumbers(int i, StringBuilder sb = null)
+{
+	if (sb is null)
+	{
+		sb = new StringBuilder();
+	}
+
+	if (i == 1)
+	{
+		sb.Append(i.ToString() + " ");
+		return sb;
+	}
+
+	GetNumbers(i - 1, sb);
+	sb.Append(i.ToString() + " ");
+
+	return sb;
+}
+
+Console.WriteLine(GetNumbers(5).ToString());
